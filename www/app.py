488a06cd77f8b26451ad324ca8c0789319efaec8
杨育才 <1127424957@qq.com>
2018-09-15 18:05:44 +0800

    complete orm.py

--- www/app.py
+++ www/app.py
@@ -3,6 +3,11 @@ import asyncio,os,json,time
 from aiohttp import web
 from datetime import datetime
 
+async def index(req):
+    print('$$$$$ router in index method...')
+    return web.Response(body=b'<h1>web index</h1>', content_type='text/html')
+
+
 async def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
@@ -10,12 +15,6 @@ async def init(loop):
     print('Server listening http://127.0.0.1:9000')
     logging.info('server started at http://127.0.0.1:9000...')
     return server
-
-
-async def index(req):
-    print('$$$$$ router in index method...')
-    return web.Response(body=b'<h1>web index</h1>', content_type='text/html')
-
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
---
+++ www/orm.py
@@ -0,0 +1,193 @@
+#orm
+
+__author__ = '杨育才' 
+
+import asyncio, aiomysql, logging, time
+
+#连接池
+async def create_pool(loop, **kw):
+    print('create databases connection pool...')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host = kw.get('host', 'localhost'),
+        port = kw.get('port', 3306),
+        user = kw['user'],
+        password = kw['password'],
+        db = kw['db'],
+        charset = kw.get('charset', 'utf8'),
+        autocommit = kw.get('autocommit', True),
+        maxsize = kw.get('maxsize', 10),
+        minsize = kw.get('minsize', 1),
+        loop = loop
+    )
+
+#select
+async def select(sql, args=(), size=None):
+    global __pool
+    async with __pool.get() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cursor:
+            await cursor.execute(sql.replace('?', '%s'), args)
+            if size:
+                rs = cursor.fetchmany(size)
+            else:
+                rs = cursor.fetchall()
+    return rs
+
+#execute     (insert, update, delete)
+async def execute(sql, args, autocommit=True):
+    global __pool
+    async with __pool.get() as conn:
+        if not autocommit:
+            await conn.begin()
+        try:
+            async with conn.cursor(aiomysql.DictCursor) as cursor:
+                await cursor.execute(sql.replace('?', '%s'), args)
+                affectedrow = cursor.rowcount
+                if not autocommit:
+                    await conn.commit()
+        except BaseException as e:
+            if not autocommit:
+                conn.rollback()
+            raise
+    return affectedrow
+
+def create_args_string(n):
+    L = []
+    for i in range(n):
+        L.append('?')
+    return ', '.join(L)
+
+class Field(object):
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+    def __str__(self):
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
+class StringField(Field):
+    def __init__(self, name=None, column_type='varchar(100)', primary_key=None, default=None):
+        super().__init__(name, column_type, primary_key, default)
+
+class IntegerField(Field):
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+class FloatField(Field):
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+class BooleanField(Field):
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
+class TextField(Field):
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
+
+class ModelMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        if name=='Model':
+            return type.__new__(cls, name, bases, attrs)
+        table_name = attrs.get('__table__', None) or name.lower()
+        mappings = dict()
+        fields=[]
+        primary_key=None
+        for k,v in attrs.items():
+            if isinstance(v, Field):
+                if not v.name:
+                    v.name = k
+                mappings[k] = v
+                if v.primary_key:
+                    if primary_key:
+                        raise StandardError('Duplicate primary key for field: %s' % k)
+                    primary_key = k
+                else:
+                    fields.append(k)
+        if not primary_key:
+            raise StandardError('primary key is not found!')
+        for k in mappings.keys():
+            attrs.pop(k)
+        table_primary_key = mappings.get(primary_key).name
+        table_fields = list(map(lambda f:mappings.get(f).name, fields))        
+        attrs['__table__'] = table_name
+        attrs['__mappings__'] = mappings
+        attrs['__fields__'] = fields
+        attrs['__primary_key__'] = primary_key
+        attrs['__table_fields__'] = table_fields
+        attrs['__table_primary_key__'] = table_primary_key
+        attrs['__select__'] = 'select %s, %s from %s' % (table_primary_key, ', '.join(table_fields), table_name) 
+        attrs['__insert__'] = 'insert into %s(%s, %s) values(%s)' % (table_name, ', '.join(table_fields), table_primary_key, create_args_string(len(table_fields)+1))
+        attrs['__update__'] = 'update %s set %s where %s=?' % (table_name, ', '.join(list(map(lambda f:'%s=?' % f, table_fields))), table_primary_key)
+        attrs['__delete__'] = 'delete from %s where %s=?' % (table_name, table_primary_key)
+        return type.__new__(cls, name, bases, attrs)
+
+class Model(dict, metaclass=ModelMetaclass):
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+    
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, values):
+        self[key] = values
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueOrDefault(self, key):
+        value = getattr(self, key, None)
+        if not value:
+            field = self.__mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                setattr(self, key, value)
+        return value
+    
+    def printAllInfo(self):
+        for k, v in Student.__mappings__.items():
+            print(k, v, self.getValueOrDefault(k))
+
+    async def save(self):
+        '(save) save obj to database'
+        fields_values = list(map(self.getValueOrDefault, self.__fields__))
+        fields_values.append(self.getValueOrDefault(self.__primary_key__))
+        rows = await execute(self.__insert__, fields_values)
+        if rows != 1:
+            print('(save) failed insert , effected rows %s !' % rows)
+
+    @classmethod
+    async def find(cls, pk):
+        'find a row by primary_key'
+        rs = await select('%s where %s = ?' % (cls.__select__, cls.__table_primary_key__), [pk,], 1)
+        if len(rs) == 0:
+            return None
+        return cls(*rs[0])
+
+
+class Student(Model):
+    __table__ = 'student'
+    id = IntegerField('id', primary_key=True)
+    name = StringField('stu_name')
+    age = IntegerField('age')
+    score = FloatField('score')
+    time = BooleanField('time', default=time.time)
+
+#stu = Student(name='yang', age=12, score=88)
+#stu.printAllInfo()
+
+async def test(loop):
+    await create_pool(loop, user='root',password='123456', db='test')
+    stu = Student(name='杨育才', age=21, score=18)
+    await stu.save()
+
+loop = asyncio.get_event_loop()
+tasks = [test(loop)]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.run_forever()
+
+
