27d7c505f1785b3e81d50de7b6cc44e25e7d07e5
杨育才 <1127424957@qq.com>
2018-09-17 14:33:29 +0800

    增加一些handler

--- www/handlers.py
+++ www/handlers.py
@@ -1,18 +1,20 @@
 import logging; logging.basicConfig(level=logging.INFO)
 from webs import get, post
 from aiohttp import web
+from models import Blog, User, Comment
+import time
 
 @get('/')
 async def index(request):
-    logging.info('$$$$$ router in index method...')
-    return '<h1>hello index!!!!</h1>'
-
-
-@get('/hello')
-async def hello(request):
-    logging.info('$$$$$ router in hello method...')
-    return web.Response(body=b'<h1>hello</h1>', content_type='text/html')
-
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
+    return {
+        '__template__': 'blogs.html',
+        'blogs': blogs
+    }
 
 @get('/jinja2')
 async def jinja2(request):
@@ -29,3 +31,15 @@ async def jinja2(request):
         }
         ]
     }
+
+@get('/api/users')
+async def api_get_users(request):
+    users = [
+        User(id='1', name='yang', created_at=time.time()-120),
+        User(id='2', name='yucai', created_at=time.time()-3600),
+        User(id='3', name='learn python', created_at=time.time()-7200)
+    ]
+    return {
+        'users':users
+    }
+
