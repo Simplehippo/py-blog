db130afd85b198b52bcc412da9f41b3482936127
杨育才 <1127424957@qq.com>
2018-09-17 14:34:30 +0800

    测试新加入orm.py内的方法

--- www/test.py
+++ www/test.py
@@ -2,6 +2,20 @@
 
 __author__ = '杨育才'
 
+from models import User
 from config import configs
+import asyncio 
+import orm
 
-print(configs)
+async def test():
+    #初始化数据库连接池
+    await orm.create_pool(loop, **configs['db'])
+    #user = User(email='yang@qq.com', password='123456', name='yang', image='blank')
+    #await user.save()
+    u = await User.find(id=1002)
+    print('u: ', u)
+    #await u.remove()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test())
+loop.run_forever()
