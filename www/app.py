ded82613f79d3b8a5cf99dfdbecbfbeb5b6603ae
杨育才 <1127424957@qq.com>
2018-09-17 14:32:34 +0800

    增加jinja2的日期过滤器, 并为app绑定静态文件路由

--- www/app.py
+++ www/app.py
@@ -1,8 +1,8 @@
 import logging; logging.basicConfig(level=logging.INFO)
-import asyncio, json, orm, os
+import asyncio, json, orm, os, time
 from aiohttp import web
 from jinja2 import Environment,FileSystemLoader
-from webs import add_routes
+from webs import add_routes, add_static
 from config import configs
 
 def init_jinja2(app, **kw):
@@ -20,7 +20,24 @@ def init_jinja2(app, **kw):
         path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
     logging.info('set jinja2 template path: %s' % path)
     env = Environment(loader=FileSystemLoader(path), **options)
+    filters = kw.get('filters', None)
+    if filters is not None:
+        for name, f in filters.items():
+            env.filters[name] = f
     app['__jinja2_env__'] = env
+
+def datetime_filter(t):
+    delta = int(time.time() - t)
+    if delta < 60:
+        return u'1分钟前'
+    if delta < 3600:
+        return u'%s分钟前' % (delta // 60)
+    if delta < 86400:
+        return u'%s小时前' % (delta // 3600)
+    if delta < 604800:
+        return u'%s天前' % (delta // 86400)
+    dt = datetime.fromtimestamp(t)
+    return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
 async def logger_middlewares(app, handler):
     async def logger(request):
@@ -32,7 +49,7 @@ async def response_middlewares(app, handler):
     async def response(request):
         logging.info('response_middlewares handler...')
         r = await handler(request)
-        if isinstance(r, web.Response):
+        if isinstance(r, web.StreamResponse):
             return r
         if isinstance(r, bytes):
             resp = web.Response(body=r)
@@ -58,27 +75,25 @@ async def response_middlewares(app, handler):
                 return resp 
 
         #default response 
-        resp = web.Response(body=str(r).encode('utf8'))
+        resp = web.Response(body=str('默认的返回内容, 检查response_middlewares是否出错(仅用于测试)').encode('utf8'))
         resp.content_type = 'text/plain;charset=utf-8'      
         return resp
     return response
 
 async def init(loop):
     #初始化数据库连接池
-    await orm.create_pool(loop, 
-        user=configs['db']['user'], 
-        password=configs['db']['password'], 
-        db=configs['db']['database']
-    )
+    await orm.create_pool(loop, **configs['db'])
     #初始化aiohttp服务器
     app = web.Application(
         loop=loop,
         middlewares=[logger_middlewares, response_middlewares]
     )
     #初始化模板引擎
-    init_jinja2(app)
+    init_jinja2(app, filters=dict(datetime=datetime_filter))
     #扫描handlers内的handler,并自动注册
     add_routes(app, 'handlers')
+    #增加静态资源的访问
+    add_static(app)
     server = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return server
