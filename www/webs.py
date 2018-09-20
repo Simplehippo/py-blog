82477ab889a7848d8d7bfdcf319f75342f6b40b9
杨育才 <1127424957@qq.com>
2018-09-20 21:04:26 +0800

    增加fn任意参数的支持,而不仅限于request

--- www/webs.py
+++ www/webs.py
@@ -1,5 +1,7 @@
 import logging; logging.basicConfig(level=logging.INFO)
 import functools, asyncio, inspect, aiohttp, os
+from apis_error import APIError
+from urllib import parse
 
 #GET请求的装饰器
 def get(path):
@@ -18,19 +20,108 @@ def post(path):
         @functools.wraps(func)
         def wrapper(*args, **kw):
             return func(*args, **kw)
-        wrapper.__method__ = 'GET'
+        wrapper.__method__ = 'POST'
         wrapper.__route__ = path
         return wrapper
     return decorator
 
+def get_required_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
+            args.append(name)
+    return tuple(args)
+
+def get_named_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            args.append(name)
+    return tuple(args)
+
+def has_named_kw_args(fn):
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            return True
+
+def has_var_kw_arg(fn):
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.VAR_KEYWORD:
+            return True
+
+def has_request_arg(fn):
+    sig = inspect.signature(fn)
+    params = sig.parameters
+    found = False
+    for name, param in params.items():
+        if name == 'request':
+            found = True
+            continue
+        if found and (param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.KEYWORD_ONLY and param.kind != inspect.Parameter.VAR_KEYWORD):
+            raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
+    return found
+
+
 class RequestHandler(object):
     def __init__(self, fn):
         self._func = fn
+        self._has_request_arg = has_request_arg(fn)
+        self._has_var_kw_arg = has_var_kw_arg(fn)
+        self._has_named_kw_args = has_named_kw_args(fn)
+        self._named_kw_args = get_named_kw_args(fn)
+        self._required_kw_args = get_required_kw_args(fn)
     
     async def __call__(self, request):
-        #kw = ...
-        
-        return await self._func(request)
+        kw = None
+        if self._has_var_kw_arg or self._has_named_kw_args or self._has_var_kw_arg:
+            if request.method == 'POST':
+                if not request.content_type:
+                    return web.HTTPBadRequest('Miss Content-Type')
+                ct = request.content_type.lower()
+                if ct.startswith('application/json'):
+                    params = await request.json()
+                    if not isinstance(params, dict):
+                        return web.HTTPBadRequest('JSON body must be object')
+                    kw = dict(**params)
+                elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
+                    params = await request.post()
+                    kw = dict(**params)
+                else:
+                    return web.HTTPBadRequest('unsupported content_type: %s' % request.content_type)
+            if request.method == 'GET':
+                qs =request.query_string
+                if qs:
+                    kw = dict()
+                    for k, v in parse.parse_qs(qs, True).items():
+                        kw[k] = v[0]
+        if kw is None:
+            kw = dict(**request.match_info)
+        else:
+            if not self._has_var_kw_arg and self._named_kw_args:
+                copy = dict()
+                for name in self._named_kw_args:
+                    if name in kw:
+                        copy[name] = kw[name]
+                kw = copy
+        for k, v in request.match_info.items():
+            if k in kw:
+                logging.warning('Duplicate arg name in named arg and kw args:%s' % k)
+            kw[k] = v
+        if self._has_request_arg:
+            kw['request'] = request
+        if self._has_var_kw_arg:
+            for name in self._named_kw_args:
+                if name not in kw:
+                    return web.HTTPBadRequest('Missing args:%s' % name)
+        logging.info('call args: %s' % str(kw))
+        try:
+            return await self._func(**kw)
+        except APIError as e:
+            return  dict(error=e.error, data=e.data, message=e.message)
 
 def add_static(app):
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
